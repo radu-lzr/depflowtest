3eb889a907f2fcf7894ca2e91c62ed0af64f0353
Radu Lazar <radu.lazar@cellenza.com>
2026-02-20 10:35:32 +0100

    Add Terraform backend configuration and storage account resource for Azure deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,25 @@ provider "azurerm" {
   features {}
 }
 
+# tfstate stored in Azure Storage Account
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "terrateam-rg"
+    storage_account_name  = "terrateamstorageacc "
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform"
   location = "northeurope"
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "stterraformradutest"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
